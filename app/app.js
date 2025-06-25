5c16b5167e4ef5c458de761cc6f77c2456a11e42
electrohyun <dev.electrohyun@gmail.com>
2025-06-25 17:36:55 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
 
 const PORT = 3000;
@@ -12,6 +13,8 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드...
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,42 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render("home/index");
+const output = {
+    hello: (req, res) => {
+        res.render("home/index");
+    },
+    
+    login: (req, res) => {
+        res.render("home/login");
+    }, 
 }
 
-const login = (req, res) => {
-    res.render("home/login");
+const users = {
+    id: ["jkh", "전기맨", "일현"],
+    psword: ["1234", "1", "12"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        })
+    },
 }
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process
 };
